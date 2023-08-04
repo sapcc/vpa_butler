aebbf19a0827f4751b47cb17fabc35d64f5e09f6
Nuckal777 <erik.schubert@sap.com>
2023-08-04 16:17:54 +0200

    Add tests

--- internal/controllers/generic_controller_test.go
+++ internal/controllers/generic_controller_test.go
@@ -3,10 +3,12 @@ package controllers_test
 import (
 	"context"
 	"fmt"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/sapcc/vpa_butler/internal/common"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	appsv1 "k8s.io/api/apps/v1"
 	autoscalingv1 "k8s.io/api/autoscaling/v1"
@@ -18,96 +20,109 @@ import (
 	"k8s.io/utils/ptr"
 )
 
-var _ = Describe("GenericControllers", func() {
+const (
+	deploymentName string = "test-deployment"
+)
 
-	labels := map[string]string{"app": "test"}
-	selector := metav1.LabelSelector{
+var (
+	labels   = map[string]string{"app": "test"}
+	selector = metav1.LabelSelector{
 		MatchLabels: labels,
 	}
-	containers := []corev1.Container{
+	containers = []corev1.Container{
 		{
 			Name:  "test-container",
 			Image: "nginx",
 		},
 	}
+)
 
-	expectVpa := func(name string) {
-		var vpaRef types.NamespacedName
-		vpaRef.Name = name
-		vpaRef.Namespace = metav1.NamespaceDefault
-		Eventually(func() error {
-			var vpa vpav1.VerticalPodAutoscaler
-			err := k8sClient.Get(context.Background(), vpaRef, &vpa)
-			if err != nil {
-				return err
-			}
-			mangedBy, ok := vpa.Annotations[common.AnnotationManagedBy]
-			if !ok {
-				return fmt.Errorf("vpa does not have managed-by annotation")
-			}
-			if mangedBy != common.AnnotationVpaButler {
-				return fmt.Errorf("vpa has wrong managed-by annotation")
-			}
-			return nil
-		}).Should(Succeed())
-	}
-
-	deleteVpa := func(name string) {
-		var vpaRef types.NamespacedName
-		vpaRef.Name = name
-		vpaRef.Namespace = metav1.NamespaceDefault
+func expectVpa(name string) {
+	var vpaRef types.NamespacedName
+	vpaRef.Name = name
+	vpaRef.Namespace = metav1.NamespaceDefault
+	Eventually(func() error {
 		var vpa vpav1.VerticalPodAutoscaler
 		err := k8sClient.Get(context.Background(), vpaRef, &vpa)
-		if errors.IsNotFound(err) {
-			return
+		if err != nil {
+			return err
 		}
-		Expect(err).To(Succeed())
-		Expect(k8sClient.Delete(context.Background(), &vpa)).To(Succeed())
+		mangedBy, ok := vpa.Annotations[common.AnnotationManagedBy]
+		if !ok {
+			return fmt.Errorf("vpa does not have managed-by annotation")
+		}
+		if mangedBy != common.AnnotationVpaButler {
+			return fmt.Errorf("vpa has wrong managed-by annotation")
+		}
+		return nil
+	}).Should(Succeed())
+}
+
+func deleteVpa(name string) {
+	var vpaRef types.NamespacedName
+	vpaRef.Name = name
+	vpaRef.Namespace = metav1.NamespaceDefault
+	var vpa vpav1.VerticalPodAutoscaler
+	err := k8sClient.Get(context.Background(), vpaRef, &vpa)
+	if errors.IsNotFound(err) {
+		return
 	}
+	Expect(err).To(Succeed())
+	Expect(k8sClient.Delete(context.Background(), &vpa)).To(Succeed())
+}
+
+func makeDeployment() *appsv1.Deployment {
+	deployment := &appsv1.Deployment{}
+	deployment.Name = deploymentName
+	deployment.Namespace = metav1.NamespaceDefault
+	deployment.Spec.Selector = &selector
+	deployment.Spec.Template.ObjectMeta.Labels = labels
+	deployment.Spec.Replicas = ptr.To[int32](1)
+	deployment.Spec.Template.Spec.Containers = containers
+	return deployment
+}
+
+var _ = Describe("GenericController", func() {
 
 	Context("when creating a deployment", func() {
 		var deployment *appsv1.Deployment
+		var defaultUpdateMode vpav1.UpdateMode
 
 		BeforeEach(func() {
-			deployment = &appsv1.Deployment{}
-			deployment.Name = "test-deployment"
-			deployment.Namespace = metav1.NamespaceDefault
-			deployment.Spec.Selector = &selector
-			deployment.Spec.Template.ObjectMeta.Labels = labels
-			deployment.Spec.Replicas = ptr.To[int32](1)
-			deployment.Spec.Template.Spec.Containers = containers
+			defaultUpdateMode = common.VpaUpdateMode
+			deployment = makeDeployment()
 			Expect(k8sClient.Create(context.Background(), deployment)).To(Succeed())
 		})
 
 		AfterEach(func() {
 			deleteVpa("test-deployment-deployment")
 			Expect(k8sClient.Delete(context.Background(), deployment)).To(Succeed())
+			common.VpaUpdateMode = defaultUpdateMode
 		})
 
 		It("should create a vpa", func() {
 			expectVpa("test-deployment-deployment")
 		})
 
-		It("should delete its vpa if one is hand-crafted", func() {
-			var vpa vpav1.VerticalPodAutoscaler
-			vpa.Name = "test-deployment-custom-vpa"
-			vpa.Namespace = metav1.NamespaceDefault
-			vpa.Spec.TargetRef = &autoscalingv1.CrossVersionObjectReference{
-				Name:       "test-deployment",
-				Kind:       "Deployment",
-				APIVersion: "v1",
-			}
-			Expect(k8sClient.Create(context.Background(), &vpa)).To(Succeed())
-
-			Eventually(func() error {
+		It("patches the served vpa", func() {
+			// need to sleep here to ensure the a vpa is created before the update
+			// to this global variable
+			time.Sleep(100 * time.Millisecond)
+			common.VpaUpdateMode = vpav1.UpdateModeAuto
+			unmodified := deployment.DeepCopy()
+			deployment.Spec.Replicas = ptr.To[int32](2)
+			Expect(k8sClient.Patch(context.Background(), deployment, client.MergeFrom(unmodified))).To(Succeed())
+			Eventually(func() vpav1.UpdateMode {
 				var vpa vpav1.VerticalPodAutoscaler
 				err := k8sClient.Get(context.Background(), types.NamespacedName{
 					Name:      "test-deployment-deployment",
 					Namespace: metav1.NamespaceDefault,
 				}, &vpa)
-				return err
-			}).ShouldNot(Succeed())
-			deleteVpa("test-deployment-custom-vpa")
+				if err != nil {
+					return vpav1.UpdateMode("")
+				}
+				return *vpa.Spec.UpdatePolicy.UpdateMode
+			}).Should(Equal(common.VpaUpdateMode))
 		})
 	})
 
@@ -156,4 +171,80 @@ var _ = Describe("GenericControllers", func() {
 		})
 	})
 
+	Context("when creating a hand-crafted vpa and a deployment afterwards", func() {
+		var vpa *vpav1.VerticalPodAutoscaler
+		var deployment *appsv1.Deployment
+
+		BeforeEach(func() {
+			vpa = &vpav1.VerticalPodAutoscaler{}
+			vpa.Name = "test-deployment-custom-vpa"
+			vpa.Namespace = metav1.NamespaceDefault
+			vpa.Spec.TargetRef = &autoscalingv1.CrossVersionObjectReference{
+				Name:       deploymentName,
+				Kind:       "Deployment",
+				APIVersion: "v1",
+			}
+			Expect(k8sClient.Create(context.Background(), vpa)).To(Succeed())
+			deployment = makeDeployment()
+			Expect(k8sClient.Create(context.Background(), deployment)).To(Succeed())
+		})
+
+		AfterEach(func() {
+			Expect(k8sClient.Delete(context.Background(), deployment)).To(Succeed())
+			deleteVpa("test-deployment-custom-vpa")
+			deleteVpa("test-deployment-deployment")
+		})
+
+		It("does not serve a vpa", func() {
+			var vpa vpav1.VerticalPodAutoscaler
+			err := k8sClient.Get(context.Background(), types.NamespacedName{
+				Name:      "test-deployment-deployment",
+				Namespace: metav1.NamespaceDefault,
+			}, &vpa)
+			Expect(err).To(HaveOccurred())
+			Expect(errors.IsNotFound(err)).To(BeTrue())
+		})
+
+	})
+
+})
+
+var _ = Describe("VpaController", func() {
+
+	Context("when creating a deployment and a hand-crafted vpa afterwards", func() {
+		var deployment *appsv1.Deployment
+		var vpa *vpav1.VerticalPodAutoscaler
+
+		BeforeEach(func() {
+			deployment = makeDeployment()
+			Expect(k8sClient.Create(context.Background(), deployment)).To(Succeed())
+			vpa = &vpav1.VerticalPodAutoscaler{}
+			vpa.Name = "test-deployment-custom-vpa"
+			vpa.Namespace = metav1.NamespaceDefault
+			vpa.Spec.TargetRef = &autoscalingv1.CrossVersionObjectReference{
+				Name:       deploymentName,
+				Kind:       "Deployment",
+				APIVersion: "v1",
+			}
+			Expect(k8sClient.Create(context.Background(), vpa)).To(Succeed())
+		})
+
+		AfterEach(func() {
+			deleteVpa("test-deployment-custom-vpa")
+			deleteVpa("test-deployment-deployment")
+			Expect(k8sClient.Delete(context.Background(), deployment)).To(Succeed())
+		})
+
+		It("should delete the served vpa", func() {
+			Eventually(func() error {
+				var vpa vpav1.VerticalPodAutoscaler
+				err := k8sClient.Get(context.Background(), types.NamespacedName{
+					Name:      "test-deployment-deployment",
+					Namespace: metav1.NamespaceDefault,
+				}, &vpa)
+				return err
+			}).ShouldNot(Succeed())
+		})
+
+	})
 })
