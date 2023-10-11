b3c7c88ae67bb73abbbebfba2225c3f1a5d7e023
Nuckal777 <erik.schubert@sap.com>
2023-10-11 17:34:19 +0200

    Fix health check

--- cmd/vpa_butler/main.go
+++ cmd/vpa_butler/main.go
@@ -16,6 +16,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -105,6 +106,8 @@ func main() {
 		Log:             mgr.GetLogger().WithName("vpa-runnable"),
 	}
 	handleError(mgr.Add(&vpaRunnable), "unable to add vpa runnable")
+	handleError(mgr.AddHealthzCheck("healthz", healthz.Ping), "unable to set up health check")
+	handleError(mgr.AddReadyzCheck("readyz", healthz.Ping), "unable to set up ready check")
 	setupLog.Info("starting manager")
 	handleError(mgr.Start(ctrl.SetupSignalHandler()), "problem running manager")
 }
